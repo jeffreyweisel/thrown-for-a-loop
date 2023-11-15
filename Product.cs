13e2c98eca5f60e2b3f9f12da02b685a13ba3f92
Jeffrey Weisel <jeffrey.weisel@kbwtech.com>
2023-11-15 11:22:09 -0600

    final

--- Product.cs
+++ Product.cs
@@ -1,10 +1,13 @@
 public class Product
 {  
-     public string Quality { get; set; }
     public string Name { get; set; }
-    public int Price { get; set; }
+    public decimal Price { get; set; }
     public bool Sold { get; set; }
     public DateTime StockDate { get; set; }
-    public int ManufactureYear { get; set; }
+    public int ManufacturerYear { get; set; }
+
+    public double Condition { get; set; }
+
+    
 
 }
--- Program.cs
+++ Program.cs
@@ -1,72 +1,169 @@
-﻿string greeting = @"Welcome to Thrown For  A Loop
-Your one-stop shop for used sporting equipment";    //declare string to be referenced later
-
-Console.WriteLine(greeting);    //same as console.log in js
-
-List<Product> products = new List<Product>()
+﻿List<Product> products = new List<Product>()    //creating new list syntax
 {
-    new Product()
-    {
-        Name = "Football",
-        Price = 15,
-        Sold = false,
-        Quality = "decent",
-        StockDate = new DateTime(2022, 10, 20),
-        ManufactureYear = 2010
-
-    },
-    new Product()
-    {
-        Name = "Hockey Stick",
-        Price = 12,
-        Sold = false,
-        Quality = "ehhhh",
-        StockDate = new DateTime(2023, 11, 20),
-        ManufactureYear = 2017
-    },
-    new Product()
-    {
-        Name = "Basketball Net",
-        Price = 11,
-        Sold = true,
-        Quality = "great",
-        StockDate = new DateTime(2012, 6, 21),
-        ManufactureYear = 2013
-    },
-    new Product()
-    {
-        Name = "Baseball Glove",
-        Price = 21,
-        Sold = false,
-        Quality = "okay",
-        StockDate = new DateTime(2020, 3, 15),
-        ManufactureYear = 2019
-    }
+new Product()
+{
+Name = "Football",
+Price = 15.00M,
+Sold = false,
+StockDate = new DateTime(2022, 10 ,2),
+ManufacturerYear = 2010,
+Condition = 4.2
+},
+new Product()
+{
+Name = "Hockey Stick",
+Price = 12.99M,
+Sold = false,
+StockDate = new DateTime(2023, 1, 4),
+ManufacturerYear = 2020,
+Condition = 2.5
+},
+new Product()
+{
+Name = "Soccer Ball",
+Price = 7.50M,
+Sold = false,
+StockDate = new DateTime(2021, 3, 9),
+ManufacturerYear = 2012,
+Condition = 3
+},
+new Product()
+{
+Name = "Basketball Net",
+Price = 10.99M,
+Sold = false,
+StockDate = new DateTime(2023, 11, 2),
+ManufacturerYear = 2014,
+Condition = 5
+},
+new Product()
+{
+Name = "Shoulder Pads",
+Price = 23.75M,
+Sold = true,
+StockDate = new DateTime(2003, 1, 1),
+ManufacturerYear = 2002,
+Condition = 4.9
+},
+new Product()
+{
+Name = "Tennis Racket",
+Price = 5.60M,
+Sold = false,
+StockDate = new DateTime(2021, 7, 11),
+ManufacturerYear = 2014,
+Condition = 3.6
+}
 };
 
-Console.WriteLine("Products:");
+DateTime now = DateTime.Now;    //declaring now as DateTime type and setting it to current DateTime
 
-for (int i = 0; i < products.Count; i++)
+string greeting = @"Welcome to Thrown For a Loop    
+Your one-stop shop for used sporting equipment"; //declare greeting as type string to be called later
+
+Console.WriteLine(greeting);
+string choice = null;
+while (choice != "0")
 {
-    Console.WriteLine($"{i + 1}. {products[i].Name}");
+    Console.WriteLine(@"Choose an option:
+0. Exit
+1. View All Products
+2. View Product Details
+3. View Latest Products");
+    choice = Console.ReadLine();   //setting choice = user input
+    if (choice == "0")
+    {
+        Console.WriteLine("See ya!");
+    }
+    else if (choice == "1")
+    {
+        ListProducts();
+    }
+    else if (choice == "2")
+    {
+        ViewProductDetails();
+    }
+    else if (choice == "3")
+    {
+        ViewLatestProducts();
+    }
 }
-Console.WriteLine("Please enter a product number: ");
 
-int response = int.Parse(Console.ReadLine().Trim());    //trim removes whitespace
-
-while (response > products.Count || response < 1)  //checks to make sure something was typed.
+void ViewProductDetails()
 {
-    Console.WriteLine("Choose a number between 1 and 5!");
-    response = int.Parse(Console.ReadLine().Trim());       //ReadLine returns line of characters from the input stream, or null if no more lines are available
-}
+    ListProducts();
 
-Product chosenProduct = products[response - 1];
-DateTime now = DateTime.Now;
+    Product chosenProduct = null;
 
-TimeSpan timeInStock = now - chosenProduct.StockDate;
+    while (chosenProduct == null)
+    {
+        Console.WriteLine("Enter a product number: ");
+        try
+        {
+            int response = int.Parse(Console.ReadLine().Trim());
+            chosenProduct = products[response - 1];
+        }
+        catch (FormatException)
+        {
+            Console.WriteLine("Please type only integers!");
+        }
+        catch (ArgumentOutOfRangeException)
+        {
+            Console.WriteLine("Please choose an existing item only!");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex);
+            Console.WriteLine("Do Better!");
+        }
+    }
 
-Console.WriteLine(@$"You chose: 
+    TimeSpan timeInStock = now - chosenProduct.StockDate;
+
+    Console.WriteLine(@$"You chose: 
 {chosenProduct.Name}, which costs {chosenProduct.Price} dollars.
-It is {now.Year - chosenProduct.ManufactureYear} years old. 
-It {(chosenProduct.Sold ? "is not available." : $"has been in stock for {timeInStock.Days} days, and is of {chosenProduct.Quality} quality.")}");
+It is {now.Year - chosenProduct.ManufacturerYear} years old.
+It {(chosenProduct.Sold ? "is not available." : $"has been in stock for {timeInStock.Days} days.")}");
+}
+
+void ListProducts()
+{
+    decimal totalValue = 0.0M;
+    foreach (Product product in products)
+    {
+        if (!product.Sold)
+        {
+            totalValue += product.Price;
+        }
+    }
+    Console.WriteLine($"Total inventory value: ${totalValue}");
+    Console.WriteLine("Products:");
+    for (int i = 0; i < products.Count; i++)
+    {
+        Console.WriteLine($"{i + 1}. {products[i].Name}");
+    }
+}
+
+void ViewLatestProducts()
+{
+    // create a new empty List to store the latest products
+    List<Product> latestProducts = new List<Product>();
+    // Calculate a DateTime 90 days in the past
+    DateTime threeMonthsAgo = DateTime.Now - TimeSpan.FromDays(90);
+    // loop through the products
+    foreach (Product product in products)
+    {
+        // Add a product to latestProducts if it fits the criteria
+        if (product.StockDate > threeMonthsAgo && !product.Sold)
+        {
+            latestProducts.Add(product);
+        }
+    }
+    Console.WriteLine("The Latest Products Are:");
+    // print out the latest products to the console
+    for (int i = 0; i < latestProducts.Count; i++)
+    {
+        Console.WriteLine($"{i + 1}. {latestProducts[i].Name}");
+    }
+}
 
